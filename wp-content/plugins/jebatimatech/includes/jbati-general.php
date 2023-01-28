df8a722994c6f371ee2d7d223abdafd1eefc7c43
chrispaganon <info@chrispaganon.com>
2023-01-28 11:54:58 -0500

    Register custom post type jbati_solutions

--- wp-content/plugins/jebatimatech/includes/jbati-general.php
+++ wp-content/plugins/jebatimatech/includes/jbati-general.php
@@ -1,2 +1,21 @@
 <?php
 if ( !defined( 'ABSPATH' ) ) exit;
+
+add_action('init', 'jbati_register_software_post_type');
+
+function jbati_register_software_post_type() {
+  $labels = array(
+    'name'               => __( 'Solutions', 'jebatimatech' ),
+    'singular_name'      => __( 'Solution', 'jebatimatech' ),
+    'menu_name'          => __( 'Solutions', 'jebatimatech' )
+  );
+    $args = array(
+    'labels'        => $labels,
+    'description'   => 'Logiciels et équipements',
+    'public'        => true,
+    'menu_position' => 5,
+    'supports'      => array( 'title', 'editor', 'comments', 'revisions', 'author', 'excerpt', 'thumbnail', 'custom-fields' ),
+    'has_archive'   => true,
+  );
+  register_post_type( 'jbati_solutions', $args );
+}
