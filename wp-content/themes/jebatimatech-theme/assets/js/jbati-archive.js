254f0d3c7679373529695cfd2bae683cd8ce4165
chrispaganon <info@chrispaganon.com>
2023-01-29 19:06:33 -0500

    Start real filtering logic & add comments for later

--- wp-content/themes/jebatimatech-theme/assets/js/jbati-archive.js
+++ wp-content/themes/jebatimatech-theme/assets/js/jbati-archive.js
@@ -4,23 +4,47 @@ jQuery(document).ready(function( $ ) {
 
   filters.forEach(filter => {
     filter.filter_items.forEach( filter_item => {
-      $(`#filter-${filter.slug} #filter-item-${filter_item.id}`).click( event => filterItemClicked(event) )
+      $(`#filter-${filter.slug} #filter-item-${filter_item.id}`).click( filter_item, () => {
+        // Update active to true OR false
+        filter_item.active = true
+        filterItemClicked(filter_item)
+      })
     })
   })
 
-  function filterItemClicked(event) {
-    // Update the 'active' value of filter_items instead. Then update solutions kind of like below. But each solution will need to be checked for every active filter_item.
+  function filterItemClicked(filter_item) {
+    // Each solution needs to be checked for every active filter_item instead
+    // const active_filters = getActiveFilters()
     solutions = solutions.map( solution => {
-      if ( solution.categories.some(c => c.term_id == event.target.value ) ) {
+      // active_filters.forEach ...
+      if ( solution.categories.some(c => c.term_id == filter_item.id ) ) {
         solution.active = false
       } else {
         solution.active = true
       }
       return solution
     })
+    // update the DOM here
+    // updateSolutions()
     console.log('filtered solutions', solutions)
+    console.log('active filters', filters)
+  }
+
+  function getActiveFilters() {
+    // To filter solutions easier
+    return active_filters_only
+  }
+
+  function updateSolutions() {
+    $('.solution-item').forEach( solution_item => {
+      // get the id
+      // check if active
+      // update css class accordingly
+    })
   }
 })
+
+
 
 // Loop through each filter
 // Attach callback to each filter item
