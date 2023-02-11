fa79d061ac601d56ebe56fb7f3602b12dcb7350b
chrispaganon <info@chrispaganon.com>
2023-02-11 17:14:04 -0500

    Add some styles for the solutions boxes

--- wp-content/themes/jebatimatech-theme/content-jbati_solutions.php
+++ wp-content/themes/jebatimatech-theme/content-jbati_solutions.php
@@ -36,7 +36,7 @@ foreach ($args['properties'] as $property) {
       <?php esc_html_e($args['content'], 'jebatimatech'); ?>
     </div>
     <div class="solution-footer">
-      <h4>Fonctionalitées</h4>
+      <h4>Fonctionalités</h4>
       <ul>
         <li><span class="checkmark <?php echo esc_attr(get_functionality_class($args, 'productivite')) ?>">✓</span>Productivité</li>
         <li><span class="checkmark <?php echo esc_attr(get_functionality_class($args, 'securite')) ?>">✓</span>Sécurité</li>
