2b418657f115f08ad5e9b0b12fe33ab3f61941d0
chrispaganon <info@chrispaganon.com>
2023-02-11 16:59:40 -0500

    Do not show the irrelevant filter items

--- wp-content/themes/jebatimatech-theme/content-jbati_filter.php
+++ wp-content/themes/jebatimatech-theme/content-jbati_filter.php
@@ -8,10 +8,12 @@ if (!defined('ABSPATH')) exit;
   <?php endif ?>
   <div class="filter-items-wrapper jbati-accordion-content <?php if (!$args['show_title']) echo 'active'; ?>">
     <?php foreach ($args['filter_items'] as $filter_item) : ?>
-      <div class="filter-item-wrapper">
-        <input type="checkbox" class="checkbox" id="filter-item-<?php echo esc_attr($filter_item['slug']); ?>" value="filter-item-<?php echo esc_attr($filter_item['slug']); ?>">
-        <label class="label" for="filter-item-<?php echo esc_attr($filter_item['slug']); ?>"><?php esc_html_e($filter_item['label'], 'jebatimatech'); ?></label>
-      </div>
+      <?php if($filter_item['show']) : ?>
+        <div class="filter-item-wrapper">
+          <input type="checkbox" class="checkbox" id="filter-item-<?php echo esc_attr($filter_item['slug']); ?>" value="filter-item-<?php echo esc_attr($filter_item['slug']); ?>">
+          <label class="label" for="filter-item-<?php echo esc_attr($filter_item['slug']); ?>"><?php esc_html_e($filter_item['label'], 'jebatimatech'); ?></label>
+        </div>
+      <?php endif ?>
     <?php endforeach ?>
   </div>
 </div>
