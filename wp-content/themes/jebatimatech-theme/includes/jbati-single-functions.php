ced9ef82906eafd233f5e93fbd5ba090e270be9a
chrispaganon <info@chrispaganon.com>
2023-03-05 16:13:30 -0500

    Add & style comments rating

--- wp-content/themes/jebatimatech-theme/includes/jbati-single-functions.php
+++ wp-content/themes/jebatimatech-theme/includes/jbati-single-functions.php
@@ -13,3 +13,51 @@ function jbati_overwrite_comment_fields( $fields ) {
   $fields['email'] = str_replace( 'Email', 'Courriel', $fields['email'] );
   return $fields;
 }
+
+
+/**
+ * Add the rating field to the comment form
+ */
+add_action( 'comment_form_top', 'jbati_add_star_rating' );
+
+function jbati_add_star_rating() {
+  ?>
+  <div class="star-rating-wrapper">
+    <input class="star star-5" id="star-5" type="radio" value="5" name="jbati_rating"/>
+    <label class="star star-5" for="star-5"></label>
+    <input class="star star-4" id="star-4" type="radio" value="4" name="jbati_rating"/>
+    <label class="star star-4" for="star-4"></label>
+    <input class="star star-3" id="star-3" type="radio" value="3" name="jbati_rating"/>
+    <label class="star star-3" for="star-3"></label>
+    <input class="star star-2" id="star-2" type="radio" value="2" name="jbati_rating"/>
+    <label class="star star-2" for="star-2"></label>
+    <input class="star star-1" id="star-1" type="radio" value="1" name="jbati_rating"/>
+    <label class="star star-1" for="star-1"></label>
+  </div>
+  <?php
+}
+
+
+/**
+ * Validate the comment rating
+ */
+add_filter( 'pre_comment_on_post', 'jbati_validate_comment_rating', 10, 1 );
+
+function jbati_validate_comment_rating( $comment_post_id ) {
+  error_log(print_r($_POST, true));
+  if ( ! isset( $_POST['jbati_rating'] ) || empty($_POST['jbati_rating']) ) {
+    wp_die( __( 'Erreur: veuillez donner une note à la solution.', 'jebatimatech' ) );
+  }
+}
+
+
+/**
+ * Save the comment rating along with comment
+ */
+add_action( 'comment_post', 'jbati_save_comment_rating' );
+
+function jbati_save_comment_rating( $comment_id ) {
+  if ( isset( $_POST['jbati_rating'] ) && ! empty($_POST['jbati_rating']) ) {
+    add_comment_meta( $comment_id, 'jbati_rating', $_POST['jbati_rating'], true );
+  }
+}
