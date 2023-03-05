13f64da03ee03ce4b75873f9575d74b9c96b37d0
chrispaganon <info@chrispaganon.com>
2023-03-05 12:01:43 -0500

    Fix search display and searchbar style

--- wp-content/themes/jebatimatech-theme/functions.php
+++ wp-content/themes/jebatimatech-theme/functions.php
@@ -20,7 +20,7 @@ function jbati_theme_enqueue_styles() {
   wp_enqueue_style('jebatimatech-theme-css', get_stylesheet_directory_uri() . '/style.css', array('generate-style'), rand(111, 9999));
   wp_enqueue_style('jebatimatech-theme-general-css', get_stylesheet_directory_uri() . '/assets/css/jebatimatech-theme-general.css', array('jebatimatech-theme-css'), rand(111, 9999));
   wp_enqueue_style('jbati-single-solutions-css', get_stylesheet_directory_uri() . '/assets/css/jbati-single-solutions.css', array('jebatimatech-theme-general-css'), rand(111, 9999));
-  if (is_archive()) {
+  if (is_archive() || is_search()) {
     wp_enqueue_style('jbati-archive-solutions-css', get_stylesheet_directory_uri() . '/assets/css/jbati-archive-solutions.css', array('jebatimatech-theme-general-css'), rand(111, 9999));
   }
 }
