3a1725270006275d310444c61087b105a5ae68e1
chrispaganon <info@chrispaganon.com>
2023-03-05 16:48:36 -0500

    Move the extra-filter-controls above the filters

--- wp-content/themes/jebatimatech-theme/templates/content-jbati_archive.php
+++ wp-content/themes/jebatimatech-theme/templates/content-jbati_archive.php
@@ -13,27 +13,30 @@ $filters = apply_filters( 'jbati_filters_array', array(), $solutions );
 do_action( 'jbati_before_archive', $solutions, $filters );
 ?>
 
-<main class="site-main jabti-solutions-archive-grid" id="main">
-  <div class="jbati-solutions-archive-sidebar sidebar is-left-sidebar" id="left-sidebar">
-    <div class="inside-left-sidebar">
-      <h2 class="filters-title">Filtrer par:</h2>
+<main class="site-main" id="main">
+  <div class="jbati-extra-filter-controls-wrapper">
+    <div class="jbati-active-filters-pills"></div>
+    <?php get_template_part( 'templates/component', 'ca_switch', [] ); ?>
+  </div>
+
+  <div class="jabti-solutions-archive-grid">
+    <div class="jbati-solutions-archive-sidebar sidebar is-left-sidebar" id="left-sidebar">
+      <div class="inside-left-sidebar">
+        <h2 class="filters-title">Filtrer par:</h2>
+        <?php 
+        foreach ( $filters as $filter) : 
+          get_template_part( 'templates/content', 'jbati_filter', $filter );
+        endforeach
+        ?>
+      </div>
+    </div>
+
+    <div class="jbati-solutions-archive-content content-area" id="primary">
       <?php 
-      foreach ( $filters as $filter) : 
-        get_template_part( 'templates/content', 'jbati_filter', $filter );
+      foreach ( $solutions as $solution) : 
+        get_template_part( 'templates/content', 'jbati_solutions', $solution );
       endforeach
       ?>
     </div>
-  </div>
-
-  <div class="jbati-solutions-archive-content content-area" id="primary">
-    <div class="jbati-extra-filter-controls-wrapper">
-      <div class="jbati-active-filters-pills"></div>
-      <?php get_template_part( 'templates/component', 'ca_switch', [] ); ?>
-    </div>
-    <?php 
-    foreach ( $solutions as $solution) : 
-      get_template_part( 'templates/content', 'jbati_solutions', $solution );
-    endforeach
-    ?>
   </div>
 </main>
