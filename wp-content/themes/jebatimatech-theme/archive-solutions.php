6178dcd890b86c8b1ebd918bf78f282a284c1cec
chrispaganon <info@chrispaganon.com>
2023-03-24 18:34:05 +0100

    Modify the archive solutions intro text

--- wp-content/themes/jebatimatech-theme/archive-solutions.php
+++ wp-content/themes/jebatimatech-theme/archive-solutions.php
@@ -11,8 +11,8 @@ get_header();
 ?>
 
 <div class="jbati-intro">
-  <h1>Logiciels pour la construction</h1>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <h1>Logiciels et équipements pour la construction</h1>
+  <p>Parcourez notre lexique dès maintenant pour découvrir des termes clés et des définitions utiles ! Enrichissez votre vocabulaire technologique et trouvez les solutions qui répondent à vos besoins.</p>
 </div>
 
 <?php
