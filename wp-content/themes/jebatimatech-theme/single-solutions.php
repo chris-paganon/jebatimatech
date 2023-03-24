096f4d879433b077e3d03b744079f4993636a053
chrispaganon <info@chrispaganon.com>
2023-03-24 11:40:21 +0100

    Move pricing out of solution-content

--- wp-content/themes/jebatimatech-theme/single-solutions.php
+++ wp-content/themes/jebatimatech-theme/single-solutions.php
@@ -34,24 +34,14 @@ get_header(); ?>
 		</section>
 
 		<section class="solution-section solution-content has-side-content">
-			<div class="section-content">
-				<div class="section-main-content">
-					<h3><?php esc_html_e('EN SAVOIR PLUS SUR BIOLIFT', 'jebatimatech'); ?></h3>
-					<?php the_content(); ?>
-					<div class="content-language">
-						<?php echo jbati_get_terms_list('pays_origine', get_the_ID()); ?>
-						<?php echo jbati_get_terms_list('langue_du_service', get_the_ID()); ?>
-					</div>
-					<p><?php echo esc_html__('Sur le marché depuis:', 'jebatimatech') . ' '. get_field('existence_technologie'); ?></p>
+			<div class="section-content-vertical">
+				<h3><?php esc_html_e('EN SAVOIR PLUS', 'jebatimatech'); ?></h3>
+				<?php the_content(); ?>
+				<div class="content-language">
+					<?php echo jbati_get_terms_list('pays_origine', get_the_ID()); ?>
+					<?php echo jbati_get_terms_list('langue_du_service', get_the_ID()); ?>
 				</div>
-				<div class="section-side-content price-sub-section">
-					<h4><?php esc_html_e('Options de tarifications dès:', 'jebatimatech'); ?></h4>
-					<?php echo jbati_get_acf_field_value('mode_de_tarification', get_the_ID(), true); ?>
-					<?php echo jbati_get_acf_field_value('modulation_tarification', get_the_ID(), true); ?>
-					<?php echo jbati_get_terms_list('delais_demarrage', get_the_ID(), true); ?>
-					<?php echo jbati_get_terms_list('essais_gratuits', get_the_ID(), false); ?>
-					<?php echo jbati_get_terms_list('tarification_achat', get_the_ID(), true); ?>
-				</div>
+				<p><?php echo esc_html__('Sur le marché depuis:', 'jebatimatech') . ' '. get_field('existence_technologie'); ?></p>
 			</div>
 		</section>
 
@@ -91,6 +81,17 @@ get_header(); ?>
 				<?php echo jbati_get_terms_list('import_export', get_the_ID()); ?>
 				<?php echo jbati_get_acf_field_value('pourcent_quebecois_service', get_the_ID()); ?>
 				<?php echo jbati_get_acf_field_value('solution_complementaire', get_the_ID()); ?>
+			</div>
+		</section>
+
+		<section class="solution-section solution-pricing">
+			<h2><?php esc_html_e('Tarification', 'jebatimatech'); ?></h2>
+			<div class="section-content jbati-3-columns">
+				<?php echo jbati_get_acf_field_value('mode_de_tarification', get_the_ID(), true); ?>
+				<?php echo jbati_get_acf_field_value('modulation_tarification', get_the_ID(), true); ?>
+				<?php echo jbati_get_terms_list('delais_demarrage', get_the_ID(), true); ?>
+				<?php echo jbati_get_terms_list('essais_gratuits', get_the_ID(), false); ?>
+				<?php echo jbati_get_terms_list('tarification_achat', get_the_ID(), true); ?>
 			</div>
 		</section>
 
