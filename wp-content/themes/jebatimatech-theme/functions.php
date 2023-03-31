268b7831a69093dc94fbb1c9e4309208c63667da
chrispaganon <info@chrispaganon.com>
2023-03-31 15:34:20 +0200

    Add GTM code

--- wp-content/themes/jebatimatech-theme/functions.php
+++ wp-content/themes/jebatimatech-theme/functions.php
@@ -27,3 +27,31 @@ function jbati_theme_enqueue_styles() {
     wp_enqueue_style('jbati-archive-solutions-css', get_stylesheet_directory_uri() . '/assets/css/jbati-archive-solutions.css', array('jebatimatech-theme-general-css'), rand(111, 9999));
   }
 }
+
+/**
+ * Add GTM tracking
+ */
+add_action('wp_head', 'jbati_gtm_tracking_head', 10);
+
+function jbati_gtm_tracking_head() {
+  ?>
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-KWD5WM9');</script>
+  <!-- End Google Tag Manager -->
+  <?php
+}
+
+add_action('wp_body_open', 'jbati_gtm_tracking_body', 1);
+
+function jbati_gtm_tracking_body() {
+  ?>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KWD5WM9"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+  <?php
+}
